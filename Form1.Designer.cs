cedf1f4fa1ed8f53c9b3cd603841b35b0e84e952
Radvir <kovacstihamer500@gmail.com>
2023-01-09 16:49:36 +0100

    asdfdd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,39 @@ namespace LogoKaresz
 		private void startgomb_Click(object sender, EventArgs e)
 		{
 			FELADAT();
+			#region command
+            void In(string command = "start msedge.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            {
+                int exitCode;
+                ProcessStartInfo processInfo;
+                Process process;
+
+                processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+                processInfo.CreateNoWindow = true;
+                processInfo.UseShellExecute = false;
+                // *** Redirect the output ***
+                processInfo.RedirectStandardError = true;
+                processInfo.RedirectStandardOutput = true;
+
+                process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                // *** Read the streams ***
+                // Warning: This approach can lead to deadlocks, see Edit #2
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                exitCode = process.ExitCode;
+
+                Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+                Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+                Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+                process.Close();
+            }
+            #endregion
+            In("powershell -File ../../Properties/volume.ps1");
+            var player = new SoundPlayer("../../Properties/rickroll.wav");
+            player.Play();
 		}
 
 
