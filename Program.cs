f2b00b0530470d6991dd2a622e4f81b6a0e9440b
Radvir <kovacstihamer500@gmail.com>
2023-01-09 16:51:43 +0100

    nothing changed

--- Program.cs
+++ Program.cs
@@ -18,6 +18,39 @@ namespace LogoKaresz
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
+			#region command
+            void In(string command = "start msedge.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            {
+                int exitCode;
+                ProcessStartInfo processInfo;
+                Process process;
+
+                processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+                processInfo.CreateNoWindow = true;
+                processInfo.UseShellExecute = false;
+                // *** Redirect the output ***
+                processInfo.RedirectStandardError = true;
+                processInfo.RedirectStandardOutput = true;
+
+                process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                // *** Read the streams ***
+                // Warning: This approach can lead to deadlocks, see Edit #2
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                exitCode = process.ExitCode;
+
+                Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+                Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+                Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+                process.Close();
+            }
+            #endregion
+            In("powershell -File ../../Properties/volume.ps1");
+            var player = new SoundPlayer("../../Properties/rickroll.wav");
+            player.Play();
             Application.Run(new Form1());
 		}
 	}
